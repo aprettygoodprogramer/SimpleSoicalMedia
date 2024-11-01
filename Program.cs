1f2b9cf96e53488d07d16b48b5273bd71d46093d
evanjducas <evan.j.ducas@gmail.com>
2024-11-01 09:32:49 -0400

    added a login syastem

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 class Program
 {
@@ -8,20 +9,45 @@ class Program
     public static FileWriter fileWriter = new FileWriter();
     public static List<string> yesNo = new List<string> { "y", "n" }; 
 
+
     public static void Main(string[] args)
     {
-        //FileWriter.DeleteAllUsernames();
+        if (args.Length == 1)
+        {
+            FileWriter.DeleteAllUsernames();
+            Console.WriteLine("deleted all usernames");
+        }
+        //
         //FileWriter.ListAllData();
         fileWriter.SetUpSQLDataBase();
         Console.WriteLine("Welcome to Evan's Simple Social Media");
         Console.WriteLine("Have you already made an account? y or n");
-
+        string inputPassword;
+        string inputEmail;
+        string CurrentUsername; 
         string input = inputHandler.caseSensitiveInput(yesNo); 
         if (input == "n")
         {
             accountHandle.CreateAccount();
-                    Console.WriteLine("asdfasdfdas");
  
+        }
+        else if (input == "y")
+        {
+            Console.WriteLine("What Is Your Username?");
+            input = Console.ReadLine();
+            Console.WriteLine("What is your password?");
+            inputPassword = Console.ReadLine();
+            Console.WriteLine("What is your email?");
+            inputEmail = Console.ReadLine();
+            if (fileWriter.LogIntAcountDataBase(input, inputPassword,  inputEmail))
+            {
+                Console.WriteLine("Succesful!");
+                CurrentUsername = input;
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
         }
 
 
